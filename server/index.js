497be370fd0b1e706e72d6069d0c4225d1850872
Georg Dronov <dronovgeorg72@gmail.com>
2024-08-11 11:19:41 +0300

    fix get and post res

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 const db = require("./models");
 
 // Routers
--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { Posts } = require("../models");
 
-router.get("/", (req, res) => {
-  res.json("Hello world");
+router.get("/", async (req, res) => {
+  const listOfPosts = await Posts.findAll();
+  res.json(listOfPosts);
 });
 
 router.post("/", async (req, res) => {
-  const psot = req.body;
+  const post = req.body;
   await Posts.create(post);
   res.json(post);
 });
